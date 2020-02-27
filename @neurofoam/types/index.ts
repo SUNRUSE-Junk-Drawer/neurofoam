7aa1a335dcfa767c96035c37510100d0717206d6
jameswilddev <jameswilddev@outlook.com>
2020-02-27 19:29:42 +0000

    Export missing types.

--- @neurofoam/types/index.ts
+++ @neurofoam/types/index.ts
@@ -2,4 +2,8 @@ export { default as Json } from "./json"
 
 export { default as RequestResult } from "./request-result"
 
+export { default as PersistenceResult } from "./persistence-result"
+
+export { default as Persistence } from "./persistence"
+
 export { default as Application } from "./application"
