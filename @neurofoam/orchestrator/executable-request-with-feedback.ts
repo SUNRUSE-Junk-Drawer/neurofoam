f6675299d75602fdbe35c33de1b9489dfedbf090
jameswilddev <jameswilddev@outlook.com>
2020-04-12 12:50:09 +0100

    Add type to wrap request with additional information.

---
+++ @neurofoam/orchestrator/executable-request-with-feedback.ts
@@ -0,0 +1,11 @@
+import * as neurofoamTypes from "@neurofoam/types"
+import ExecutableRequest from "./executable-request"
+
+type ExecutableRequestWithFeedback<
+  TRequest extends neurofoamTypes.Json
+  > = {
+    readonly executableRequest: ExecutableRequest<TRequest>
+    readonly sessionUuidIsNew: boolean
+  }
+
+export default ExecutableRequestWithFeedback
