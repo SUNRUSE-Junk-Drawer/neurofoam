be1439d26aa0e8598861db7f8e2adbc15ff226f3
jameswilddev <jameswilddev@outlook.com>
2020-04-12 12:46:00 +0100

    Add helper method for determing whether the accept-charset header is valid.

---
+++ @neurofoam/channel-node-http/is-accept-charset-header-valid.ts
@@ -0,0 +1,15 @@
+const regex = /(?:^|,)\s*(?:utf-8|\*)\s*(?:$|;|,)/i
+
+export default function (
+  acceptCharset: undefined | string,
+): boolean {
+  if (acceptCharset === undefined) {
+    return true
+  }
+
+  if (acceptCharset.trim() === ``) {
+    return true
+  }
+
+  return regex.test(acceptCharset)
+}
