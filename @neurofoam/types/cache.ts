f71aa4870d79aeb7853962b92967ad1929cc6781
jameswilddev <jameswilddev@outlook.com>
2020-10-04 15:45:13 +0100

    Remove cache type.

--- @neurofoam/types/cache.ts
+++
@@ -1,8 +0,0 @@
-import Json from "./json";
-
-type Cache<TValue extends Json> = {
-  get(key: string): Promise<undefined | TValue>;
-  set(key: string, value: TValue): Promise<void>;
-};
-
-export default Cache;
